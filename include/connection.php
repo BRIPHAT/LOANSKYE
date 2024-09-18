7a4929c87ad99fcf04b0a448c564df7a25f8f7df
BRIPHAT <brianmtaita@gmail.com>
2024-09-18 09:40:45 -0700

    first commit

---
+++ include/connection.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "high_loan_services";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$conn) {
+    die('connection failed' . mysqli_connect_error());
+} else {
+    echo "connection succefull";
+}
