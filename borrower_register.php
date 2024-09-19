88bfcdd1242514fb2f09e9fa7dcb4310029f27d9
BRIPHAT <brianmtaita@gmail.com>
2024-09-19 05:39:13 -0700

    19 SEOT 2024 15:37PM

---
+++ borrower_register.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOANSKYE</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body class="register-body">
+    <form action="control/register_control.php" method="post" autocomplete="off">
+        <fieldset class="background-register">
+            <fieldset class=" register">
+                <h1>REGISTER IN LOANSKYE</h1>
+                <label for=" Firstname">Firstname:</label>
+                <input type="text" name="Firstname" id="Firstname" placeholder="enter first name" size="30" required="required"><br>
+                <label for=" Lastname">Lastname:</label>
+                <input type="text" name="Lastname" id="Lastname" placeholder="enter last name" size="30" required="required"><br>
+                <label for=" Phone_number">Phone Number:</label>
+                <input type="number" minlength="10" maxlength="10" size="30" name="Phone_number" id=" Phone_number" placeholder="+xxx xxx xxx xxx" required="required"><br>
+                <label for=" Physical_address">Address:</label>
+                <input type="text" name="Pysical_address" id="Pysical_address" size="32" required="required" placeholder="address of your house"><br>
+                <label for=" Email">Email:</label>
+                <input type="email" name="Email" id="Email" size="34" required="required" placeholder="exmple@gmail.com"><br>
+                <label for=" date_of_birth">Date Of Birth</label>
+                <input type="date" name="date_of_birth" id="date_of_birth" max="2006-01-01" required="required"><br>
+                <label for=" country">Country:</label>
+                <input type="text" name="country" id="country" required="required" placeholder="enter your country lived" size="32"><br>
+                <label for="city">City</label>
+                <input type="text" name="city" id="city" placeholder="Enter your city" size="37"><br>
+                <label for="street">Street:</label>
+                <input type="text" name="street" id="street" placeholder="Enter your street" size="35"><br>
+                <label for="house_number">Number Of House:</label>
+                <input type="number" name="house_number" id="house_number" required="required" size="30" placeholder="enter house number"><br>
+                <label for="ownership">Asset and Non-asset Owner:</label>
+                <input type="text" name="ownership" id="ownership" size="16" required="required" title="asset and non asset used for apply loan" placeholder="asset and non asset used for apply loan"><br>
+                <label for="Profile">Profile:</label>
+                <input type="file" name="Profile" id="Profile" required="required" title="enter a document with pdf format only"><br>
+                <label for="National_Identification">National Identification:</label>
+                <input type="number" minlength="20" name="National_Identification" id=" National_Identification" required="required" placeholder="National number ID"><br>
+                <label for="credit_score">Credit:</label>
+                <input type="number" name="credit_score" id="credit_score" placeholder="enter salary amount taken per month" title="enter salary amount taken per month" required="required"><br>
+                <label for="sponsor_name">Sponsor Name:</label>
+                <input type="text" name="sponsor_name" id="sponsor_name" required="required" placeholder="enter your sponsor name" size="28"><br>
+
+
+
+            </fieldset>
+        </fieldset>
+    </form>
+</body>
+
+</html>
