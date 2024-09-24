a4de71f391b8e31ecb150fa513e331b3c53dbda9
BRIPHAT <brianmtaita@gmail.com>
2024-09-24 06:59:48 -0700

    4th commit

--- control/register_control.php
+++ control/register_control.php
@@ -8,22 +8,27 @@ if (isset($_POST['submit'])) {
     $password = md5($_POST['password']);
     $confirm_password = md5($_POST['confirm_password']);
 
-    $sql = "SELECT * FROM loan_applicant WHERE Email='$Email'";
+    $sql = "SELECT Email, Phone_number FROM loan_applicant WHERE Email='$Email'";
     $query = mysqli_query($conn, $sql);
     if (mysqli_num_rows($query) > 0) {
-        header('location:../borrower_register.php', "user already exist");
+        header("location:../borrower_register.php?error=1");
     } else {
 
         $sql2 = "INSERT INTO loan_applicant(Firstname,Phone_number,Email,password,confirm_password)
             VALUES('$Firstname','$Phone_number','$Email','$password','$confirm_password')";
         $query2 = mysqli_query($conn, $sql2);
         if ($query2) {
-            header("location: login.php?submited=1");
+            header("location:login.php?submited=1");
         }
 
+
         //$sql = "CALL insertBorrowerData('" . $Firstname . "', '" . $Phone_number . "','" . $Email . "','" . $password . "','" . $confirm_password . "')";
-        if (isset($_GET["submit"])) {
-            echo '<script>alert("data submited")</script>';
+        if (isset($_GET["error"])) {
+            echo '<script>alert("user already exist")</script>';
+        } else {
+            if (isset($_GET["submit"])) {
+                echo '<script>alert("data submited")</script>';
+            }
         }
     }
 }
