084794a87f57fb1281cd654c709ae5a4fdca9daf
BRIPHAT <brianmtaita@gmail.com>
2024-09-30 01:50:54 -0700

    commit

---
+++ include/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOANSKYE</title>
+</head>
+
+<body>
+    <header>
+        <nav class="header">
+            <li><a href="#"></a></li>
+        </nav>
+    </header>
+</body>
+
+</html>
